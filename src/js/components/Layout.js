e5249ef4c653438d6c7f99dc0fea6113c1f47483
DAI KAWAI <kdmgs110@gmail.com>
2022-03-07 00:11:05 +0900

    propsを学習

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,13 +3,26 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+    constructor (){
+        super();
+        this.state = {
+            name: "DAI"
+        };
+    }
     render() {
+        setTimeout(
+            () => {
+                this.setState({name: "KAWAI"});
+            }, 1000
+        );
+        const title = "Welcome DAI"
         return (
             <div>
-                <Header />
+                {this.state.name}
+                <Header title={title} />
                 <Footer />
             </div>
 
-);
+    );
     }
 }
